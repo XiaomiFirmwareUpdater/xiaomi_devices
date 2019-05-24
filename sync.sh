58d74fb5639d914508ae559ca92e6166e7dc0ef8
yshalsager <ysh-alsager@hotmail.com>
2019-05-24 22:25:35 +0200

    sync: fix git add derp

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 pip3 install requests bs4
 python3 gsmarena.py
-git add gsmarena.json && git -c "user.name=XiaomiFirmwareUpdater" -c "user.email=xiaomifirmwareupdater@gmail.com" commit -m "Sync: $(date +%d.%m.%Y) [skip ci]"
+git add devices.json all/*.json && git -c "user.name=XiaomiFirmwareUpdater" -c "user.email=xiaomifirmwareupdater@gmail.com" commit -m "Sync: $(date +%d.%m.%Y) [skip ci]"
 git push https://$XFU@github.com/XiaomiFirmwareUpdater/xiaomi_devices.git HEAD:gsmarena
  
