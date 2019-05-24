f7a34743de7cb0b0dbcf90fdfb303439a17e5ef0
yshalsager <ysh-alsager@hotmail.com>
2019-05-24 20:33:45 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Xiaomi Devices - Google Play Certified
+This branch contains all Xiaomi devices scrapped info, from [Google Play Certified devices list](https://github.com/androidtrackers/certified-android-devices) and updated always.
---
+++ gplay.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3.7
+"""Xiaomi Devices Info Scrapper"""
+
+import json
+from requests import get
+
+
+def get_devices_list():
+    """
+    Download latest certified devices list
+    """
+    url = "https://raw.githubusercontent.com/androidtrackers/" +\
+          "certified-android-devices/master/README.md"
+    response = get(url)
+    with open('devices.md', 'wb') as out:
+        out.write(response.content)
+
+
+def extract_info():
+    """
+    Extract Xiaomi devices info and write json file
+    """
+    with open('devices.md', 'r') as file:
+        data = file.readlines()
+    devices = []
+    for line in data:
+        info = {}
+        details = {}
+        if line.startswith('|Xiaomi|'):
+            name = line.split('|')[2]
+            codename = line.split('|')[3]
+            model = line.split('|')[4]
+            details.update({'name': name})
+            details.update({'model': model})
+            info.update({codename: details})
+            devices.append(info)
+    with open('devices.json', 'w') as output:
+        json.dump(devices, output, indent=1)
+
+
+def main():
+    """
+    Make jsom file of Xiaomi devices info from Google Play certified devices list
+    """
+    get_devices_list()
+    extract_info()
+
+
+if __name__ == '__main__':
+    main()
