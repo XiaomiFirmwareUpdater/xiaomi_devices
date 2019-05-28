7f911f969f54ad7a6013b6a573fc16738457534a
yshalsager <ysh-alsager@hotmail.com>
2019-05-28 20:14:47 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Xiaomi Devices - codenames
+This branch contains all Xiaomi devices codenames, from various sources and updated always.
---
+++ codenames.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3.7
+"""Xiaomi Devices codenames"""
+
+import json
+from requests import get
+
+DEVICES = []
+
+
+def master():
+    """
+    extract codenames form master branch list
+    """
+    url = 'https://raw.githubusercontent.com/XiaomiFirmwareUpdater/' +\
+          'xiaomi_devices/master/devices.json'
+    data = get(url).json()['data']
+    codenames = []
+    for key in data.keys():
+        codenames.append(key)
+    for i in codenames:
+        if i:
+            DEVICES.append(i)
+
+
+def models():
+    """
+    extract codenames form models list
+    """
+    url = 'https://raw.githubusercontent.com/XiaomiFirmwareUpdater/' +\
+          'xiaomi_devices/models/models.json'
+    data = get(url).json()
+    codenames = [i['codename'] for i in data]
+    for i in codenames:
+        if i:
+            DEVICES.append(i)
+
+
+def gplay():
+    """
+    extract codenames form Google Play list
+    """
+    url = 'https://raw.githubusercontent.com/XiaomiFirmwareUpdater/' +\
+          'xiaomi_devices/gplay/devices.json'
+    data = get(url).json()
+    codenames = []
+    for i in data:
+        for key in i.keys():
+            codenames.append(key)
+    for i in codenames:
+        if i:
+            DEVICES.append(i)
+
+
+def main():
+    """
+    Generate json file of Xiaomi devices codename form various sources
+    """
+    gplay()
+    models()
+    master()
+    data = list(dict.fromkeys(DEVICES))  # remove duplicates
+    data.sort()  # sort
+    with open('codenames.json', 'w') as out:
+        json.dump(data, out, indent=1)
+
+
+if __name__ == '__main__':
+    main()
