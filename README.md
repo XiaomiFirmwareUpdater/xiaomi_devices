ab8114f6ef27baf8618be9bc4eda5309517dbd8c
yshalsager <ysh-alsager@hotmail.com>
2019-06-03 02:00:23 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Xiaomi Devices - MIUI codes
+This branch contains Xiaomi devices MIUI codes data, updated always.
---
+++ miui.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.7
+"""Xiaomi MIUI codes Info Scraper"""
+
+import json
+from collections import OrderedDict
+from requests import get
+
+
+def main():
+    """
+    Scrap Xiaomi devices MIUI codes and generate JSON files
+    """
+    current = json.loads(get(
+        "https://raw.githubusercontent.com/XiaomiFirmwareUpdater/miui-updates-tracker/master/" +
+        "stable_fastboot/stable_fastboot.json").content)
+    eol = json.loads(get(
+        "https://raw.githubusercontent.com/XiaomiFirmwareUpdater/miui-updates-tracker/master/EOL/" +
+        "stable_fastboot/stable_fastboot.json").content)
+    data = current + eol
+    info = {}
+    for i in data:
+        codename = i['codename']
+        version = i['version']
+        miui_code = version.split('.')[-1][1:3]
+        info.update({codename: miui_code})
+    sorted_data = OrderedDict(sorted(info.items()))
+    with open('miui.json', 'w') as output:
+        json.dump(sorted_data, output, indent=1)
+
+
+if __name__ == '__main__':
+    main()
