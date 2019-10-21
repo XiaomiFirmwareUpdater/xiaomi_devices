67ba07802ea40487279533e3ec7a7d231a8f2a78
yshalsager <ysh-alsager@hotmail.com>
2019-10-21 18:48:53 +0200

    yaml: switch to requirements.txt
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-pip3 install requests yaml
+pip3 install -r requirements.txt
 python3 codenames.py
 git add codenames.json && git -c "user.name=XiaomiFirmwareUpdater" -c "user.email=xiaomifirmwareupdater@gmail.com" commit -m "Sync: $(date +%d.%m.%Y) [skip ci]"
 git push https://$XFU@github.com/XiaomiFirmwareUpdater/xiaomi_devices.git HEAD:codenames
