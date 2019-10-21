d9659929a069bc459f0f11837d9367ff90be3f2b
yshalsager <ysh-alsager@hotmail.com>
2019-10-21 18:42:56 +0200

    sync: Add yaml to requirements
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-pip3 install requests
+pip3 install requests yaml
 python3 names.py
 git add names.json && git -c "user.name=XiaomiFirmwareUpdater" -c "user.email=xiaomifirmwareupdater@gmail.com" commit -m "Sync: $(date +%d.%m.%Y) [skip ci]"
 git push https://$XFU@github.com/XiaomiFirmwareUpdater/xiaomi_devices.git HEAD:names
