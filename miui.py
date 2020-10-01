0cceced7f51fc3b128a513632a8fb32c21ef142e
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2020-10-01 15:01:24 +0200

    update for V3 tracker

--- miui.py
+++ miui.py
@@ -11,20 +11,15 @@ def main():
     """
     Scrap Xiaomi devices MIUI codes and generate JSON files
     """
-    current = yaml.load(get(
-        "https://raw.githubusercontent.com/XiaomiFirmwareUpdater/miui-updates-tracker/master/" +
-        "stable_fastboot/stable_fastboot.yml").text, Loader=yaml.CLoader)
-    eol = yaml.load(get(
-        "https://raw.githubusercontent.com/XiaomiFirmwareUpdater/miui-updates-tracker/master/EOL/" +
-        "stable_fastboot/stable_fastboot.yml").text, Loader=yaml.CLoader)
-    data = current + eol
+    data = yaml.load(get(
+        "https://raw.githubusercontent.com/XiaomiFirmwareUpdater/miui-updates-tracker/master/data/latest.yml").text, Loader=yaml.CLoader)
     info = {}
     for i in data:
-        codename = i['codename']
-        version = i['version']
-        miui_code = version.split('.')[-1][1:5]
-        info.update({codename: miui_code})
-    info.update({"HM2013023": "HBMI", "lcsh92_wet_jb9_global": "HDMI"})
+        if i['branch'] != "Weekly" and i['method'] == "Recovery":
+            codename = i['codename']
+            version = i['version']
+            miui_code = version.split('.')[-1][1:5]
+            info.update({codename: miui_code})
     sorted_data = OrderedDict(sorted(info.items()))
     with open('miui.json', 'w') as output:
         json.dump(sorted_data, output, indent=1)
