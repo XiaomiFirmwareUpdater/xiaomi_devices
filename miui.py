51dc564cfab8c070f44743436252cc233566269d
yshalsager <ysh-alsager@hotmail.com>
2022-09-10 14:06:30 +0200

    fix global fastboot
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- miui.py
+++ miui.py
@@ -12,12 +12,14 @@ def china_devices():
     :param url: MIUI downloads page
     """
     response = get("http://www.miui.com/download.html")
-    page = BeautifulSoup(response.content, 'html.parser')
+    page = BeautifulSoup(response.content, "html.parser")
     data = page.select("script")
-    data = [str(i) for i in data if "var phones" in str(i)][0].split('=')[1].split(';')[0]
+    data = (
+        [str(i) for i in data if "var phones" in str(i)][0].split("=")[1].split(";")[0]
+    )
     info = json.loads(data)
-    sorted_info = sorted(info, key=lambda k: k['pid'], reverse=True)
-    with open('china.json', 'w') as output:
+    sorted_info = sorted(info, key=lambda k: k["pid"], reverse=True)
+    with open("china.json", "w") as output:
         json.dump(sorted_info, output, indent=1, ensure_ascii=False)
 
 
@@ -26,51 +28,66 @@ def global_devices():
     fetch MIUI downloads devices
     """
     headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Connection': 'keep-alive',
-        'Origin': 'https://new.c.mi.com',
-        'Referer': 'https://new.c.mi.com/',
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Connection": "keep-alive",
+        "Origin": "https://new.c.mi.com",
+        "Referer": "https://new.c.mi.com/",
     }
 
-    url = 'https://sgp-api.buy.mi.com/bbs/api/global/phone/getphonelist'
-    data = get(url, headers=headers).json()['data']['phone_data']['phone_list']
+    url = "https://sgp-api.buy.mi.com/bbs/api/global/phone/getphonelist"
+    data = get(url, headers=headers).json()["data"]["phone_data"]["phone_list"]
     unified = []
     for item in data:
-        if 'http://' in item['pic_url']:
-            item['pic_url'] = item['pic_url'].replace('http://', 'https://')
+        if "http://" in item["pic_url"]:
+            item["pic_url"] = item["pic_url"].replace("http://", "https://")
         unified.append(data)
-    with open('c_mi.json', 'w') as output:
+    with open("c_mi.json", "w") as output:
         json.dump(data, output, indent=1, ensure_ascii=False)
 
 
 def china_fastboot():
     """fetch MIUI china fastboot rom devices"""
-    page = BeautifulSoup(get("https://www.miui.com/shuaji-393.html", headers={'User-Agent': 'Mozilla/5.0 (Linux; U; Android 10; zh-cn; M2007J1SC Build/QKQ1.200419.002) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.141 Mobile Safari/537.36 XiaoMi/MiuiBrowser/12.8.25'}).content, 'html.parser')
-    links = [f"{i['href'].split('=')[1].split('&')[0].strip()} - {i['href'].split('=')[2].split('&')[0]}"
-             for i in page.findAll('a') if "fullromdownload" in str(i)]
-    with open("chinese_fastboot.txt", 'w') as output:
-        output.writelines(i + '\n' for i in sorted(links))
+    page = BeautifulSoup(
+        get(
+            "https://www.miui.com/shuaji-393.html",
+            headers={
+                "User-Agent": "Mozilla/5.0 (Linux; U; Android 10; zh-cn; M2007J1SC Build/QKQ1.200419.002) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.141 Mobile Safari/537.36 XiaoMi/MiuiBrowser/12.8.25"
+            },
+        ).content,
+        "html.parser",
+    )
+    links = [
+        f"{i['href'].split('=')[1].split('&')[0].strip()} - {i['href'].split('=')[2].split('&')[0]}"
+        for i in page.findAll("a")
+        if "fullromdownload" in str(i)
+    ]
+    with open("chinese_fastboot.txt", "w") as output:
+        output.writelines(i + "\n" for i in sorted(links))
 
 
 def global_fastboot():
     """fetch MIUI global fastboot rom devices"""
-    data = get("https://sgp-api.buy.mi.com/bbs/api/global/phone/getlinepackagelist").json()["data"]
-    links = [f"{i['package_url'].split('=')[1].split('&')[0].strip()} - {i['package_url'].split('=')[2].split('&')[0]}"
-             for i in data]
-    with open("global_fastboot.txt", 'w') as output:
-        output.writelines(i + '\n' for i in sorted(links))
+    data = get(
+        "https://sgp-api.buy.mi.com/bbs/api/global/phone/getlinepackagelist"
+    ).json()["data"]
+    links = [
+        f'{"_".join(i["key"].split("_")[:-2])} - {i["key"].split("_")[-1]}'
+        for i in data
+    ]
+    with open("global_fastboot.txt", "w") as output:
+        output.writelines(i + "\n" for i in sorted(links))
 
 
 def main():
     """
     Scrap Xiaomi devices downloads info from official site and generate JSON files
     """
-    #china_devices()
+    # china_devices()
     global_devices()
-    #china_fastboot()
+    # china_fastboot()
     global_fastboot()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
