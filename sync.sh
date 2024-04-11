31293c4294a112b40922f856cf833c01e8bd3058
asalde_le1 <57564314+asaldele1@users.noreply.github.com>
2024-04-11 20:50:40 +0300

    Update gsmarena script

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-pip3 install requests bs4
+pip3 install -r requirements.txt
 python3 gsmarena.py
 git add devices.json all/*.json && git -c "user.name=XiaomiFirmwareUpdater" -c "user.email=xiaomifirmwareupdater@gmail.com" commit -m "Sync: $(date +%d.%m.%Y) [skip ci]"
-git push https://$XFU@github.com/XiaomiFirmwareUpdater/xiaomi_devices.git HEAD:gsmarena 
+git push https://$XFU@github.com/asaldele1/xiaomi_devices.git HEAD:gsmarena 
