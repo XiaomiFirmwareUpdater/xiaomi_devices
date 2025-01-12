291199fa00729326b5936677e7dd050560f9c4f7
Kevin Huang <844958997@qq.com>
2025-01-12 21:45:09 +0800

    Fix duplicate name, fix multiple marketing names for models (#16)

--- models.py
+++ models.py
@@ -4,6 +4,7 @@
 import re
 import json
 from requests import get
+from collections import defaultdict
 
 DEVICES = {}
 
@@ -33,16 +34,23 @@ def main():
             name = details[0].split(']')[1].split('(')[0].strip()
         except IndexError:
             name = details[0].split(':')[0].strip()
+
+        # Clean up spaces in names
+        name = re.sub(r'\s*/\s*', '/', name)
+
         models = details[1].replace('\n\n', '\n').strip().splitlines()
-        models_ = {}
+        models_ = defaultdict(list)  # Using list to handle multiple names for the same model
         for i in models:
             model = i.split(':')[0].strip()
             model_name = i.split(':')[1].strip()
-            models_[model] = model_name
+            models_[model].append(model_name)
+
+        # Combine model names with slash if duplicates exist
+        final_models = {model: '/'.join(names) for model, names in models_.items()}
 
         info.update({"internal_name": internal})
         info.update({"name": name})
-        info.update({"models": models_})
+        info.update({"models": final_models})
 
         if codename in DEVICES:
             existing_internal_names = DEVICES[codename]['internal_name'].split('/')
@@ -55,13 +63,16 @@ def main():
 
             DEVICES[codename]['internal_name'] = '/'.join(existing_internal_names)
             DEVICES[codename]['name'] = '/'.join(existing_names)
-            DEVICES[codename]['models'] = {**DEVICES[codename]['models'], **models_}
+
+            for model, model_name in final_models.items():
+                if model in DEVICES[codename]['models']:
+                    existing_model_name = DEVICES[codename]['models'][model]
+                    if model_name not in existing_model_name:
+                        DEVICES[codename]['models'][model] = f"{existing_model_name} / {model_name}"
+                else:
+                    DEVICES[codename]['models'][model] = model_name
         else:
             DEVICES[codename] = info
-
-    # Clean up spaces in names
-    for device in DEVICES.values():
-        device['name'] = '/'.join(part.strip() for part in device['name'].split('/'))
 
     with open('models.json', 'w') as output:
         json.dump(DEVICES, output, indent=1)
